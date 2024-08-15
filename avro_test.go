7ab5258addd51e17c32ce851108c2443902bbf0c
Jonas Weile <jonas.weile@tryg.dk>
2024-08-15 18:25:49 +0000

    testing cont.

--- avro_test.go
+++ avro_test.go
@@ -80,78 +80,87 @@ var avscManifestFile = avro.MustParse(`{
 	]
 }`)
 
-func TestAvroToIceberg(t *testing.T) {
-	expected := iceberg.NewSchema(0, []iceberg.NestedField{
-		{ID: 500, Name: "manifest_path", Type: iceberg.StringType{}, Required: true, Doc: "Location URI with FS scheme"},
-		{ID: 501, Name: "manifest_length", Type: iceberg.Int64Type{}, Required: true, Doc: "Total file size in bytes"},
-		{ID: 502, Name: "partition_spec_id", Type: iceberg.Int32Type{}, Required: true, Doc: "Spec ID used to write"},
-		{ID: 517, Name: "content", Type: iceberg.Int32Type{}, Required: true, Doc: "Contents of the manifest: 0=data, 1=deletes"},
-		{
-			ID:       515,
-			Name:     "sequence_number",
-			Type:     iceberg.Int64Type{},
-			Required: true,
-			Doc:      "Sequence number when the manifest was added",
-		},
-		{
-			ID:       516,
-			Name:     "min_sequence_number",
-			Type:     iceberg.Int64Type{},
-			Required: true,
-			Doc:      "Lowest sequence number in the manifest",
-		},
-		{ID: 503, Name: "added_snapshot_id", Type: iceberg.Int64Type{}, Required: true, Doc: "Snapshot ID that added the manifest"},
-		{ID: 504, Name: "added_files_count", Type: iceberg.Int32Type{}, Required: true, Doc: "Added entry count"},
-		{ID: 505, Name: "existing_files_count", Type: iceberg.Int32Type{}, Required: true, Doc: "Existing entry count"},
-		{ID: 506, Name: "deleted_files_count", Type: iceberg.Int32Type{}, Required: true, Doc: "Deleted entry count"},
-		{ID: 512, Name: "added_rows_count", Type: iceberg.Int64Type{}, Required: true, Doc: "Added rows count"},
-		{ID: 513, Name: "existing_rows_count", Type: iceberg.Int64Type{}, Required: true, Doc: "Existing rows count"},
-		{ID: 514, Name: "deleted_rows_count", Type: iceberg.Int64Type{}, Required: true, Doc: "Deleted rows count"},
-		{
-			ID:   507,
-			Name: "partitions",
-			Type: &iceberg.ListType{
-				ElementID: 508,
-				Element: &iceberg.StructType{
-					FieldList: []iceberg.NestedField{
-						{
-							ID:       509,
-							Name:     "contains_null",
-							Type:     iceberg.BooleanType{},
-							Required: true,
-							Doc:      "True if any file has a null partition value",
-						},
-						{
-							ID:       518,
-							Name:     "contains_nan",
-							Type:     iceberg.BooleanType{},
-							Required: false,
-							Doc:      "True if any file has a nan partition value",
-						},
-						{
-							ID:       510,
-							Name:     "lower_bound",
-							Type:     iceberg.BinaryType{},
-							Required: false,
-							Doc:      "Partition lower bound for all files",
-						},
-						{
-							ID:       511,
-							Name:     "upper_bound",
-							Type:     iceberg.BinaryType{},
-							Required: false,
-							Doc:      "Partition upper bound for all files",
-						},
+var icebscgManifestFile = iceberg.NewSchema(0, []iceberg.NestedField{
+	{ID: 500, Name: "manifest_path", Type: iceberg.StringType{}, Required: true, Doc: "Location URI with FS scheme"},
+	{ID: 501, Name: "manifest_length", Type: iceberg.Int64Type{}, Required: true, Doc: "Total file size in bytes"},
+	{ID: 502, Name: "partition_spec_id", Type: iceberg.Int32Type{}, Required: true, Doc: "Spec ID used to write"},
+	{ID: 517, Name: "content", Type: iceberg.Int32Type{}, Required: true, Doc: "Contents of the manifest: 0=data, 1=deletes"},
+	{
+		ID:       515,
+		Name:     "sequence_number",
+		Type:     iceberg.Int64Type{},
+		Required: true,
+		Doc:      "Sequence number when the manifest was added",
+	},
+	{
+		ID:       516,
+		Name:     "min_sequence_number",
+		Type:     iceberg.Int64Type{},
+		Required: true,
+		Doc:      "Lowest sequence number in the manifest",
+	},
+	{ID: 503, Name: "added_snapshot_id", Type: iceberg.Int64Type{}, Required: true, Doc: "Snapshot ID that added the manifest"},
+	{ID: 504, Name: "added_files_count", Type: iceberg.Int32Type{}, Required: true, Doc: "Added entry count"},
+	{ID: 505, Name: "existing_files_count", Type: iceberg.Int32Type{}, Required: true, Doc: "Existing entry count"},
+	{ID: 506, Name: "deleted_files_count", Type: iceberg.Int32Type{}, Required: true, Doc: "Deleted entry count"},
+	{ID: 512, Name: "added_rows_count", Type: iceberg.Int64Type{}, Required: true, Doc: "Added rows count"},
+	{ID: 513, Name: "existing_rows_count", Type: iceberg.Int64Type{}, Required: true, Doc: "Existing rows count"},
+	{ID: 514, Name: "deleted_rows_count", Type: iceberg.Int64Type{}, Required: true, Doc: "Deleted rows count"},
+	{
+		ID:   507,
+		Name: "partitions",
+		Type: &iceberg.ListType{
+			ElementID: 508,
+			Element: &iceberg.StructType{
+				FieldList: []iceberg.NestedField{
+					{
+						ID:       509,
+						Name:     "contains_null",
+						Type:     iceberg.BooleanType{},
+						Required: true,
+						Doc:      "True if any file has a null partition value",
+					},
+					{
+						ID:       518,
+						Name:     "contains_nan",
+						Type:     iceberg.BooleanType{},
+						Required: false,
+						Doc:      "True if any file has a nan partition value",
+					},
+					{
+						ID:       510,
+						Name:     "lower_bound",
+						Type:     iceberg.BinaryType{},
+						Required: false,
+						Doc:      "Partition lower bound for all files",
+					},
+					{
+						ID:       511,
+						Name:     "upper_bound",
+						Type:     iceberg.BinaryType{},
+						Required: false,
+						Doc:      "Partition upper bound for all files",
 					},
 				},
-				ElementRequired: true,
 			},
-			Required: false,
-			Doc:      "Summary for each partition",
+			ElementRequired: true,
 		},
-	}...)
-	icebergSchema, err := iceberg.AvroToIceberg(avscManifestFile)
+		Required: false,
+		Doc:      "Summary for each partition",
+	},
+}...)
+
+func TestAvroManifestToIceberg(t *testing.T) {
+	expected := icebscgManifestFile
+	actual, err := iceberg.AvroToIceberg(avscManifestFile)
 	assert.NoError(t, err)
-	assert.Equal(t, expected.ID, icebergSchema.ID)
-	assert.Equal(t, expected.Fields(), icebergSchema.Fields())
+	assert.Equal(t, expected.ID, actual.ID)
+	assert.Equal(t, expected.Fields(), actual.Fields())
+}
+
+func TestIcebergManifestToAvro(t *testing.T) {
+	expected := avscManifestFile
+	actual, err := iceberg.IcebergToAvro("manifest_file", icebscgManifestFile)
+	assert.NoError(t, err)
+	assert.Equal(t, expected.String(), actual.String())
 }
